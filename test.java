da9ff283b5acaf19b34f4c9b5073c18374c47100
Xin Cheng <aui.cheng.010@gmail.com>
2018-01-18 19:44:35 -0800

    test git config

--- test.java
+++ test.java
@@ -9,17 +9,7 @@ class Employee {
 public class test{
     //System.out.println();
 
-    public static void main(String[] args) {
-        Employee em = new Employee();
-        em.age = 100;
-        System.out.println(em.age);
-        changeEmployee(em);
-        System.out.println(em.age);
-        Stack<String> test = new Stack<>();
-        for(String item : test){
-            System.out.println(item);
-        }
-    }
+
 
     public static void changeEmployee(Employee em){
         em = new Employee();
